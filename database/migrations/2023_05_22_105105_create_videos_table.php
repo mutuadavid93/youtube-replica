12f43bf3950f8f6217f78bb6bd98c378259e5881
David Mutua <mutuadavi@gmail.com>
2023-05-25 12:19:17 +0300

    ft: setup migrations seeders

--- database/migrations/2023_05_22_105105_create_videos_table.php
+++ database/migrations/2023_05_22_105105_create_videos_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('videos', function (Blueprint $table) {
             $table->id();
+            $table->text('title');
+            $table->text('video');
+            $table->text('thumbnail');
+            $table->text('user');
+            $table->text('views');
             $table->timestamps();
         });
     }
--- resources/js/Pages/Video.vue
+++ resources/js/Pages/Video.vue
@@ -16,8 +16,8 @@
     <div class="xl:flex">
       <!-- First column -->
       <div class="p-3">
-        <video src="/videos/seoul-traffic.mp4" controls autoplay />
-        <div class="text-white text-2xl font-extrabold mt-4">A cool video</div>
+        <video :src="video.video || ''" controls autoplay />
+        <div class="text-white text-2xl font-extrabold mt-4">{{ video.title }}</div>
         <div class="flex items-center mb-4">
           <img
             class="rounded-full mt-1.5 flex items-baseline w-8 h-8"
@@ -27,14 +27,22 @@
           />
           <div class="pl-2 mt-1">
             <div class="text-white text-lg font-extrabold flex items-center">
-              John Doe <CheckCircle fillColor="#888888" :size="17" />
+              {{ video.user }} <CheckCircle fillColor="#888888" :size="17" />
             </div>
-            <div class="text-sm text-gray-400 font-extrabold">1k views - 3 days ago</div>
+            <div class="text-sm text-gray-400 font-extrabold">{{ video.views }}</div>
+          </div>
+        </div>
+
+        <div class="w-[500px] p-3 block sm:hidden">
+          <div v-for="vid in recommededVideos" :key="vid">
+            <Link class="flex mb-3" :href="route('videos.show', { id: vid.id })">
+              <RecommededVideos :vid="vid" />
+            </Link>
           </div>
         </div>
 
         <div class="bg-[#3F3F3F] rounded-lg w-full p-3 text-white">
-          <div class="text-white text-lg font-extrabold">1k views - 3 days ago</div>
+          <div class="text-white text-lg font-extrabold">{{ video.views }}</div>
           <div class="text-sm font-extrabold mb-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </div>
@@ -47,38 +55,37 @@
         </div>
 
         <div class="mt-6">
-          <div class="text-white text-lg font-extrabold">12 Comments</div>
+          <div class="text-white text-lg font-extrabold">
+            {{ comments.length }} Comments
+          </div>
           <!-- stretches the flex items to fill the container along the cross-axis
           useful when you want the items to expand vertically to match the height of the container.
            -->
-          <div class="flex items-flex mb-4 mt-2">
-            <img
-              class="rounded-full mt-2 w-12 h-12"
-              :src="
-                `https://picsum.photos/id/${(Math.random() * 100).toFixed(0)}/100` || ''
-              "
-            />
-            <div class="pl-6 mt-1">
-              <div class="text-white font-extrabold flex items-baseline">
-                <div>Alex Smith</div>
-                <div class="text-gray-400 pl-3">6 days ago</div>
-              </div>
-
-              <div class="text-gray-200 text-sm font-semibold">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo amet impedit
-                soluta ea iusto odio culpa quos laboriosam, numquam quibusdam, possimus
-                autem nisi, ut neque reprehenderit tempora aut id dolorem. Sed ab iure
-                dicta soluta vel, id mollitia. Eligendi placeat quos vitae illo expedita?
-                Illo voluptatum quia error iste tempore commodi inventore ullam, magni
-                sunt nulla quaerat excepturi harum omnis?
-              </div>
-
-              <div class="mt-4 flex items-center">
-                <ThumbUpOutline fillColor="#FFFFFF" :size="20" class="pr-2" />
-                <div class="text-gray-400 text-sm font-extrabold pr-10">
-                  {{ (Math.random() * 100).toFixed(0) }}
+          <div v-for="comment in comments" :key="comment">
+            <div class="flex items-flex mb-4 mt-2">
+              <img
+                class="rounded-full mt-2 w-12 h-12"
+                :src="
+                  `https://picsum.photos/id/${(Math.random() * 100).toFixed(0)}/100` || ''
+                "
+              />
+              <div class="pl-6 mt-1">
+                <div class="text-white font-extrabold flex items-baseline">
+                  <div>{{ comment.user }}</div>
+                  <div class="text-gray-400 pl-3">{{ comment.time }}</div>
                 </div>
-                <ThumbDownOutline fillColor="#FFFFFF" :size="20" />
+
+                <div class="text-gray-200 text-sm font-semibold">
+                  {{ comment.text }}
+                </div>
+
+                <div class="mt-4 flex items-center">
+                  <ThumbUpOutline fillColor="#FFFFFF" :size="20" class="pr-2" />
+                  <div class="text-gray-400 text-sm font-extrabold pr-10">
+                    {{ (Math.random() * 100).toFixed(0) }}
+                  </div>
+                  <ThumbDownOutline fillColor="#FFFFFF" :size="20" />
+                </div>
               </div>
             </div>
           </div>
@@ -91,16 +98,10 @@
       on larger screens, you can apply both classes like this
        -->
       <div class="w-[500px] p-3 sm:block hidden">
-        <div class="flex mb-3">
-          <RecommededVideos
-            :vid="{
-              title: 'Little poodle in a jumper',
-              video: '/videos/seoul-traffic.mp4',
-              thumbnail: '/videos/Thumbnails/seoul-traffic.png',
-              user: 'John Doe',
-              views: '12k views - 3 days ago',
-            }"
-          />
+        <div v-for="vid in recommededVideos" :key="vid">
+          <Link class="flex mb-3" :href="route('videos.show', { id: vid.id })">
+            <RecommededVideos :vid="vid" />
+          </Link>
         </div>
       </div>
     </div>
@@ -115,7 +116,9 @@ import CheckCircle from "vue-material-design-icons/CheckCircle.vue";
 import ThumbUpOutline from "vue-material-design-icons/ThumbUpOutline.vue";
 import ThumbDownOutline from "vue-material-design-icons/ThumbDownOutline.vue";
 
-// defineProps({
-//   canLogin: Boolean,
-// });
+defineProps({
+  video: Object,
+  comments: Array,
+  recommededVideos: Array,
+});
 </script>
